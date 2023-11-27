24e02ce92f259c90adefd4aa52dbe4b495cff878
Nguyễn Hữu Trung Chiến <73121156+Blast15@users.noreply.github.com>
2023-11-27 10:36:30 +0700

    ok

---
+++ QLSIEUTHI1/Views/Shared/_LayoutAdmin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QLSIEUTHI1.Views.Shared
+{
+    public class _LayoutAdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
